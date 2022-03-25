93a5c1789602175d7feee175ee5b4ffb29bccd56
shk4548 <shk4548@gmail.com>
2022-03-25 09:56:51 +0900

    emailist

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -4,12 +4,48 @@ import Clock from './Clock';
 
 export default function App() {
 
+    
+    const getCurrentClockTime= function() {
+        const date = new Date();
+        const hours = date.getHours();
+        // const minutes = ('0' + date.getMinutes()).slice(-2);
+        // const seconds = ('0' + date.getSeconds()).slice(-2);
+
+        // const session = hours > 12 ? 'pm' : 'am';
+
+        return {
+            hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours -12 : hours))).slice(-2),
+            minutes : ('0' + date.getMinutes()).slice(-2),
+            seconds : ('0' + date.getSeconds()).slice(-2),
+            session : hours > 12 ? 'pm' : 'am',
+        };
+    }
+
+    const [currentClockTime, setCurrentClockTime] = useState(getCurrentClockTime);
+    const [ticks,setTicks] = useState(0);
+    
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCurrentClockTime(getCurrentClockTime)
+    //         setTicks(ticks+1)
+    //     }, 1000);
+    // }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrentClockTime(getCurrentClockTime)
+            setTicks(ticks+1)
+        }, 1000);
+    }, [currentClockTime]);
 
     return (
+        <div>
+            <span>{ticks}</span>
             <Clock
                 message={'ex05: useEffect Hook example'}
-                hours={10}
-                minutes={25}
-                seconds={40}/>
+                hours={currentClockTime.hours}
+                minutes={currentClockTime.minutes}
+                seconds={currentClockTime.seconds}/>
+        </div>
     );
 }
