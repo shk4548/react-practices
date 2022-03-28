4c6af72b48ff371bf90873929bbc3c6cdc58f83f
shk4548 <shk4548@gmail.com>
2022-03-28 10:55:12 +0900

    emaillist finish , kanbanboard start

---
+++ emaillist07/backend/src/main/java/com/example/demo/EmaillistApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class EmaillistApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmaillistApplication.class, args);
+	}
+}
---
+++ emaillist07/frontend/src/App.js
@@ -0,0 +1,108 @@
+import React, {useState, useEffect} from 'react'
+import './assets/scss/App.scss';
+import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+
+const App = () => {
+  const [emails, setEmails] = useState([]);
+
+  useEffect(async () => {
+    try{
+      const response = await fetch('/api', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      if(!response.ok){
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+
+      const json = await response.json(); // 비동기 함수
+      
+      if(json.result !== 'success'){
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      setEmails(json.data);
+    } catch(err){
+      console.log(err);
+    }
+
+      // console.log(response);
+  }, []);
+
+
+  const notifyKeywordChange = async function(keyword) {
+    console.log(`/api?kw=${keyword}`)
+    try{
+      const response = await fetch(`/api?kw=${keyword}`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
+
+      if(!response.ok){
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+
+      const json = await response.json(); // 비동기 함수
+      
+      if(json.result !== 'success'){
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      setEmails(json.data);
+    } catch(err){
+      console.log(err);
+    }
+
+  }
+
+  const notifyEmailAdd = async function(email){
+    console.log('post /api', email);
+    try{
+      const response = await fetch(`/api`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(email)
+      });
+
+      if(!response.ok){
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+
+      const json = await response.json(); // 비동기 함수
+      
+      if(json.result !== 'success'){
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      console.log(json.data);
+    } catch(err){
+      console.log(err);
+    }
+
+  }
+
+
+  return (
+    <div className={'App'}>
+      <RegisterForm callback={notifyEmailAdd}/>
+      <SearchBar callback={notifyKeywordChange}/>
+      <Emaillist emails={emails} />
+    </div>
+  )
+}
+
+export default App
